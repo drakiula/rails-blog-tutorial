22a1d60b61f93b2dee52bf0be7e9e23eaad0afe5
Dragos - Ioan Haiduc <dragos.haiduc@esolutions.ro>
2024-08-14 11:38:03 +0300

    very basic and simple security

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+  http_basic_authenticate_with name: "dhh", password: "secret", only: [:destroy, :update]
   def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
@@ -12,7 +13,7 @@ class CommentsController < ApplicationController
     redirect_to article_path(@article), status: :see_other
   end
 
-  def edit
+  def update
     @article = Article.find(params[:article_id])
     @comment = @article.comments.find(params[:id])
     @comment.status = 'archived'
